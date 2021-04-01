edd8d11ad192dee4aec95a1de7a2221609483eeb
AbdullahMQ1998 <wxf@live.de>
2021-04-01 20:19:45 +0300

    Update postComment.php

--- SWE444/postComment.php
+++ SWE444/postComment.php
@@ -15,7 +15,7 @@ $result = mysqli_query($db, $query);
 mysqli_close($db);
 
 if ($result) {
-    header("location:articleDetails.php?bookId=" . $_POST['articleId'] . "&message=Thank you for your comment");
+    header("location:articleDetails.php?articleId=" . $_POST['articleId'] . "&message=Thank you for your comment");
 } else {
    
    header("location:articleDetails.php?bookId=" . $_POST['articleId'] . "&message=An error occur while posting your comment");
